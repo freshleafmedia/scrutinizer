0804e26c7e6ceaf4ede8dff258f0eb0aa3ed11f0
Joe Strong <joestrong@gmail.com>
2017-08-30 22:13:01 +0100

    Restructured application

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/../views',
+    'twig.path' => __DIR__.'/../resources/views',
 ));
 
 $app->get('/', function() use($app) {
