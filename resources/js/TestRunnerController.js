f922e85c7628f975dd2a0d01a57fefe82c91ef5f
Joe Strong <joestrong@gmail.com>
2017-09-02 17:12:41 +0100

    Got multiple tests to run

--- resources/js/TestRunnerController.js
+++ resources/js/TestRunnerController.js
@@ -5,35 +5,37 @@ export default class TestRunnerController {
   constructor() {
     this.results = document.querySelector('#results')
     this.siteUrl = ''
-    this.tests = [
-      'tests/sitemap'
-    ]
+    this.tests = {
+      'Sitemap': 'tests/seo/sitemap',
+      'Robots.txt': 'tests/seo/robotsText',
+      'Response Time': 'tests/performance/responseTime'
+    }
   }
 
   clearResults() {
+    this.siteUrl = ''
     this.results.innerHTML = ''
   }
 
   runTests(siteUrl) {
     this.siteUrl = siteUrl
-    for (const testKey in this.tests) {
-      const endpoint = this.tests[testKey]
-      request.post({ url: endpoint, form: { url: this.siteUrl } }, (err, response, body) => {
+    for (const [testName, endpoint] of Object.entries(this.tests)) {
+      request(endpoint + "?url=" + this.siteUrl, (err, response, body) => {
         if (err || response.statusCode !== 200) {
           return
         }
-        this.addPanel(JSON.parse(body))
+        this.addPanel(testName, JSON.parse(body))
       })
     }
   }
 
-  addPanel(test) {
+  addPanel(testName, testData) {
     this.results.innerHTML += `
       <div class="mdc-card test-result-card">
         <section class="mdc-card__primary">
           <div class="test-result-card__status"></div>
-          <h1 class="mdc-card__title">${test.test}</h1>
-          <h2 class="mdc-card__subtitle">${JSON.stringify(test.results)}</h2>
+          <h1 class="mdc-card__title">${testName}</h1>
+          <h2 class="mdc-card__subtitle">${JSON.stringify(testData)}</h2>
         </section>
       </div>
     `
