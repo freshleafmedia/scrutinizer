a09c8e34f366e2b0b36cd846b13b4d4bd8b0efca
Joe Strong <joestrong@gmail.com>
2017-10-05 21:53:07 +0100

    Initialised view app

--- resources/js/ApplicationController.js
+++ resources/js/ApplicationController.js
@@ -1,10 +1,13 @@
 import TestRunnerController from './TestRunnerController.js'
 import { MDCTextfield } from '@material/textfield'
-import Vue from 'vue'
+import Vue from 'vue/dist/vue.esm.js'
 
 export default class AppController {
 
   constructor() {
+    this.vueApp = new Vue({
+      el: '#app'
+    })
     new MDCTextfield(document.querySelector('.mdc-textfield'))
     this.entry = document.querySelector('#entry')
     this.testRunner = new TestRunnerController(this)
